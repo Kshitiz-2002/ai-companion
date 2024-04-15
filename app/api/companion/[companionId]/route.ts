b1b08fb660f402da39829f4f4327a2827ae4ecd5
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-04-15 06:33:18 +0530

    feat: Chat Header

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,5 +1,5 @@
 import prismadb from "@/lib/prismadb";
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function PAT(
@@ -46,5 +46,27 @@ export async function PAT(
   } catch (error) {
     console.log("[COMPANION_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const companion = await prismadb.companion.delete({
+      where:{
+        userId,
+        id:params.companionId,
+      }
+    })
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_DELETE]", error);
+    return new Response("Server error", { status: 500 });
   }
 }
