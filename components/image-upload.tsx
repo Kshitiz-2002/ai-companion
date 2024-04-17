98ea660bf58037b7801a0e1d52ce942e2234ef35
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2024-04-17 05:45:26 +0530

    chore: fixed bug

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -1,46 +1,73 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { CldUploadButton } from 'next-cloudinary';
 import Image from "next/image";
-import  placholderImage  from "../public/placeholder.svg"
+import { useEffect, useState } from "react";
+import { CldUploadButton } from "next-cloudinary";
+
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
 
 interface ImageUploadProps {
-    value: String;
-    onChange: (src: String) => void;
-    disabled?: boolean;
+  src: string;
+  value: string;
+  onChange: (src: string) => void;
+  disabled?: boolean;
 }
 
-const ImageUpload = ({ value, onChange, disabled }: ImageUploadProps) => {
-    const [isMounted, setIsMounted] = useState(false);
-    useEffect(() => { setIsMounted(true) }, []);
+export const ImageUpload = ({
+  src,
+  value,
+  onChange,
+  disabled,
+}: ImageUploadProps) => {
+  const upload_preset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+  const [isMounted, setIsMounted] = useState(false);
+  const [newImageUrl, setNewImageUrl] = useState(src);    
 
-    if (!isMounted) {
-        return null;
-    }
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+    useEffect(() => {
+        setNewImageUrl(src);       
+        onChange(newImageUrl);     
+    }, [src]); 
+    useEffect(() => {
+    onChange(src);
+    },[src]);
+  if (!isMounted) {
+    return false;
+  }
 
-    return (
-        <div className="space-y-4 w-full flex flex-col justify-center items-center">
-            <CldUploadButton
-                onUpload={(result: any) => onChange(result.info.secure_url)}
-                options={{
-                    maxFiles: 1
-                }}
-                uploadPreset="w73cwvso"
-            >
-                <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
-                    <div className="relative h-40 w-40">
-                        <Image
-                            fill
-                            alt="Upload"
-                            src={value || placholderImage}
-                            className="rounded-lg object-cover"
-                        />
-                    </div>
-                </div>
-            </CldUploadButton>
+  return (
+    <div className="space-y-4 w-full flex flex-col justify-center items-center">
+      
+      <CldUploadButton options={{ maxFiles: 1 }} onUpload={(result: any) => onChange(result.info.secure_url)} uploadPreset={upload_preset}>
+        <div 
+          className="
+            p-4 
+            border-4 
+            border-dashed
+            border-primary/10 
+            rounded-lg 
+            hover:opacity-75 
+            transition 
+            flex 
+            flex-col 
+            space-y-2 
+            items-center 
+            justify-center
+          "
+        >
+          <div className="relative h-40 w-40">
+            <Image
+              fill
+              alt="Upload"
+              src={value ||newImageUrl|| "/placeholder.svg"}
+              className="rounded-lg object-cover"
+            />
+          </div>
         </div>
-    );
-}
-
-export default ImageUpload;
+      </CldUploadButton>
+    </div>
+  );
+};
